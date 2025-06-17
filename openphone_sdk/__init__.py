701783203588b31221aa6312e605b534c7dc841c
ili.ad <108145573+ili-ad@users.noreply.github.com>
2025-06-16 21:10:52 -0400

    Add update_contact_by_id wrapper and tests

--- openphone_sdk/__init__.py
+++ openphone_sdk/__init__.py
@@ -1,3 +1,4 @@
 from .get_call_recordings import get_call_recordings
+from .update_contact_by_id import update_contact_by_id
 
-__all__ = ["get_call_recordings"]
+__all__ = ["get_call_recordings", "update_contact_by_id"]

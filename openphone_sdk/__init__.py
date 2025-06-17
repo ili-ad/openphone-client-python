21a629508babb5d3d01c4a7a93db40e0f7015180
ili.ad <108145573+ili-ad@users.noreply.github.com>
2025-06-16 21:00:16 -0400

    Add wrapper for get call summary

--- openphone_sdk/__init__.py
+++ openphone_sdk/__init__.py
@@ -1,3 +1,4 @@
 from .get_call_recordings import get_call_recordings
+from .get_call_summary import get_call_summary
 
-__all__ = ["get_call_recordings"]
+__all__ = ["get_call_recordings", "get_call_summary"]

485b9554d2e791dda7b6c6257a570e278e422b00
ili.ad <108145573+ili-ad@users.noreply.github.com>
2025-06-16 20:14:44 -0400

    Add wrapper for contact custom fields

--- openphone_sdk/__init__.py
+++ openphone_sdk/__init__.py
@@ -1,3 +1,4 @@
 from .get_call_recordings import get_call_recordings
+from .get_contact_custom_fields import get_contact_custom_fields
 
-__all__ = ["get_call_recordings"]
+__all__ = ["get_call_recordings", "get_contact_custom_fields"]

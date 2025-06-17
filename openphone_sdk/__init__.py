f395394154b0d9c407bf20edf1731e4ff6ad6121
ili.ad <108145573+ili-ad@users.noreply.github.com>
2025-06-16 20:53:03 -0400

    Add create_message_webhook wrapper and tests

--- openphone_sdk/__init__.py
+++ openphone_sdk/__init__.py
@@ -1,3 +1,4 @@
 from .get_call_recordings import get_call_recordings
+from .create_message_webhook import create_message_webhook
 
-__all__ = ["get_call_recordings"]
+__all__ = ["get_call_recordings", "create_message_webhook"]

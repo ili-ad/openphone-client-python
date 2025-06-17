c4726e08d7fbc6c3dc865e94ab47ca0a149c54c8
ili.ad <108145573+ili-ad@users.noreply.github.com>
2025-06-16 20:03:52 -0400

    Add list conversations wrapper and tests

--- openphone_sdk/__init__.py
+++ openphone_sdk/__init__.py
@@ -1,3 +1,4 @@
 from .get_call_recordings import get_call_recordings
+from .list_conversations import list_conversations
 
-__all__ = ["get_call_recordings"]
+__all__ = ["get_call_recordings", "list_conversations"]

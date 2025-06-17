3303b1aa845f14ca3e4352b94cef24d5fd77e2fe
ili.ad <108145573+ili-ad@users.noreply.github.com>
2025-06-16 20:34:49 -0400

    Add get_call_transcript wrapper and tests

--- openphone_sdk/__init__.py
+++ openphone_sdk/__init__.py
@@ -1,3 +1,4 @@
 from .get_call_recordings import get_call_recordings
+from .get_call_transcript import get_call_transcript
 
-__all__ = ["get_call_recordings"]
+__all__ = ["get_call_recordings", "get_call_transcript"]
